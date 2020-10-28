e50f0f98b94f6e6df9c2108a0aaf301ea6d991b7
jiangnan <1063342937@qq.com>
2020-10-28 22:51:06 +0800

    git push

---
+++ IF语句.py
@@ -0,0 +1,16 @@
+car=['audi','bmw','subaru','toyota']
+if (car[0]=='audi') and (car[1].title()=='Bmw'):#使用and 并联条件
+    print("ture")
+else:
+    print(car[1].title())#if语句区分大小写
+#-------------------------------------------------
+if 1==2 or 2==3:#or只要一个满足就返回true
+    print ("just have one")
+else:
+    print("no one conform")
+#-------------------------------------------------
+#检查特定值是否在列表中
+if 'audi' in car:
+    print("yes")
+if 'Tesla' not in car:
+    print('tesla no in it.')
