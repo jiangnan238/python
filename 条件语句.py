2173c308dd9b7ba23a8092b254818720948d083c
jiangnan <1063342937@qq.com>
2020-10-28 23:14:53 +0800

    git add

--- 条件语句.py
+++ 条件语句.py
@@ -4,7 +4,7 @@ if (car[0]=='audi') and (car[1].title()=='Bmw'):#使用and 并联条件
 else:
     print(car[1].title())#if语句区分大小写
 #-------------------------------------------------
-if 1==2 or 2==3:#or只要一个满足就返回true
+if 1>2 or 2==3:#or只要一个满足就返回true
     print ("just have one")
 else:
     print("no one conform")
@@ -14,3 +14,7 @@ if 'audi' in car:
     print("yes")
 if 'Tesla' not in car:
     print('tesla no in it.')
+#------------------------------------------------
+#布尔逻辑运算
+print(car[0]=='audi')#返回true
+print(car[1]=='audi')#flase
